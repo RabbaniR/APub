a2b08baf43001bbf599b8c7f8b824b62e187ffdc
johncomposed <johncomposed@gmail.com>
2016-01-10 02:03:34 -0500

    Got some text and sketch working

--- src/app.js
+++ src/app.js
@@ -8,26 +8,66 @@ function initializeRoutes() {
   router.init();
 }
 
-//function initializeKeybindings() {}
+function initializeEvents() {
+  // Window resize
+  $( window ).resize(function() {
+    // TODO BUG this causes canvas text to disappear
+    //resizeEveryCanvas();
+  });
+}
+
 
 //function loadTextToCanvas() {}
 
+function resizeCanvas(id) {
+  var canvas = id ? $('canvas#'+ id) : $('canvas');
+  canvas.map(function(i, el){
+    el.height = $(window).height();
+    el.width = 700;
+  });
+}
+
+function addCanvas(id) {
+  $('#app').append('<canvas id="' + id + '" class="fullscreen"></canvas>');
+  resizeCanvas(id);
+  return document.getElementById(id);
+}
 
 function loadBook(book, page) {
   console.log("load book: " + book + page);
+  $('#app').empty();
 
+  var text = AnnotatedSets[book].initial_pages[page];
+  loadTextToCanvas(text);
 
+  //loadHistoryLayer();
 
+  loadAnnotationLayer();
+}
 
+function loadTextToCanvas(text) {
+  var canvas = addCanvas("Text");
 
+  CanvasTextWrapper(canvas, text, {
+		font: '16px Helvetica, sans-serif',
+		verticalAlign: 'middle',
+    textAlign: 'center',
+    justifyLines: true,
+		allowNewLine: true,
+		lineHeight: '150%'
+	});
+}
 
+function loadHistoryLayer(level, image) {
+  var canvas = addCanvas("Layer" + level);
 
 }
 
-// loadTextToCanvas() -> dom manipulation
-// loadAnnotationLayer()
+function loadAnnotationLayer() {
+  var canvas = addCanvas("Sketch");
+  $(canvas).sketch();
 
-
+}
 
 
 // loadPreviousPage()
@@ -45,10 +85,9 @@ function loadBook(book, page) {
 
 // Initialize
 $(document).ready(function() {
-
     console.log( "ready!" );
     initializeRoutes();
-    //initializeKeybindings();
+    initializeEvents();
 
 
 });
---
+++ src/assets/annotated_sets.js
@@ -0,0 +1,23 @@
+var AnnotatedSets = {};
+
+
+
+AnnotatedSets["alice"] = {
+    "metadata" : {
+        "bookname": "Alice In Wonderland",
+        "author": "Lewis Carroll",
+        "language": "English",
+        "release_date": "August 12, 2006"
+    },
+    "initial_pages": [
+      "\n      Alice was beginning to get very tired of sitting by her sister on the\n      bank, and of having nothing to do: once or twice she had peeped into the\n      book her sister was reading, but it had no pictures or conversations in\n      it, 'and what is the use of a book,' thought Alice 'without pictures or\n      conversations?'\n    ",
+       "\n      So she was considering in her own mind (as well as she could, for the hot\n      day made her feel very sleepy and stupid), whether the pleasure of making\n      a daisy-chain would be worth the trouble of getting up and picking the\n      daisies, when suddenly a White Rabbit with pink eyes ran close by her.\n    ",
+      "\n      There was nothing so <i>very</i> remarkable in that; nor did Alice think it so\n      <i>very</i> much out of the way to hear the Rabbit say to itself, 'Oh dear! Oh\n      dear! I shall be late!' (when she thought it over afterwards, it occurred\n      to her that she ought to have wondered at this, but at the time it all\n      seemed quite natural); but when the Rabbit actually <i>took a watch out of\n      its waistcoat-pocket</i>, and looked at it, and then hurried on, Alice started\n      to her feet, for it flashed across her mind that she had never before seen\n      a rabbit with either a waistcoat-pocket, or a watch to take out of it, and\n      burning with curiosity, she ran across the field after it, and fortunately\n      was just in time to see it pop down a large rabbit-hole under the hedge.\n    "
+    ],
+    "annotation_sets": [
+      {
+        "annotator": "Alice",
+        "annotated_pages": [null, null, null]
+      }
+    ]
+}
